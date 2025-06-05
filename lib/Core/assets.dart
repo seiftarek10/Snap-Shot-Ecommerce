0ced7d8eed63a5a80381648828a95667ef3ca673
Seif Tariq <SeifTariq@gmail.com>
2025-06-05 11:32:19 +0300

    add flutter gen assets extension

---
+++ lib/Core/assets.dart
@@ -0,0 +1,20 @@
+// ignore_for_file: prefer_single_quotes
+class Assets {
+  Assets._();
+
+  /// Assets for logo
+  /// assets/logo.png
+  static const String logo = "assets/logo.png";
+
+  /// Assets for splashAndroid12
+  /// assets/splash_android12.png
+  static const String splashAndroid12 = "assets/splash_android12.png";
+
+  /// Assets for splashView
+  /// assets/splash_view.png
+  static const String splashView = "assets/splash_view.png";
+
+  /// Assets for splashView Android11
+  /// assets/splash_view _android11.png
+  static const String splashViewAndroid11 = "assets/splash_view _android11.png";
+}
